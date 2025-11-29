f36637d42103f5fc77d2c31ab26c32a0bb86b4f0
Alfin0431 <alfinnn831@gmail.com>
2025-11-29 18:43:27 +0700

    paw

--- movie-react/src/main.tsx
+++ movie-react/src/main.tsx
@@ -4,7 +4,6 @@ import { RouterProvider } from 'react-router'
 import router from './routes/router'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router}/>
