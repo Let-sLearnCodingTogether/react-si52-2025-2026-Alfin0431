77583564c61ae15925372022c618068d4df8fc4e
Alfin0431 <alfinnn831@gmail.com>
2025-12-10 20:36:59 +0700

    edit

--- movie-react/src/pages/movies/EditMovie.tsx
+++ movie-react/src/pages/movies/EditMovie.tsx
@@ -1,21 +1,49 @@
-import { NavLink } from "react-router";
+import { NavLink, useNavigate, useParams } from "react-router";
 import Button from "react-bootstrap/Button"
 import Form from "react-bootstrap/Form";
-import { useState, type ChangeEvent, type FormEvent } from "react";
+import { useCallback, useEffect, useState, type ChangeEvent, type FormEvent } from "react";
 import ApiClient from "../../utils/ApiClient";
 
 interface FormMovie {
-    judul: string,
-    tahunRilis: string,
-    sutradara: string
+    judul : string,
+    tahunRilis : string,
+    sutradara : string
+}
+
+interface ResponseData {
+    data : {
+        _id : string,
+        judul : string,
+        sutradara : string,
+        tahunRilis : string,
+        createdBy : string,
+        createdAt : string,
+        updateAt : string,
+        __v : string
+    }
 }
 
 function EditMovie() {
+    const params = useParams ()
+    const navigate = useNavigate()
     const [form, setForm] =  useState<FormMovie>({
         judul: "",
         tahunRilis: "",
         sutradara: ""
     });
+
+    const fetchMovie = useCallback(async() =>{
+        const response = await ApiClient.get (`/movies/${params.id}`)
+
+        if(response.status === 200) {
+            const responseData : ResponseData = response.data
+            setForm({
+                judul : responseData.data.judul,
+                tahunRilis : responseData.data.tahunRilis,
+                sutradara : responseData.data.sutradara
+            })
+        }
+    }, [params]) 
 
     const handleInputChange = (event : ChangeEvent<HTMLInputElement>) => {
         const {name, value} = event.target
@@ -36,6 +64,10 @@ function EditMovie() {
             console.log(error);
         }
     }
+
+    useEffect (() => {
+        fetchMovie()
+    },[fetchMovie])
 
     return <div>
         <div>
